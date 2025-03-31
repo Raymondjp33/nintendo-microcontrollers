e587f2774224354fdea1155780803a568785f72f
Raymond Price <RaymondJPrice33@gmail.com>
2025-03-31 16:01:43 -0400

    Current constant values

--- scripts/bdsp/lake_duo_hunt.py
+++ scripts/bdsp/lake_duo_hunt.py
@@ -167,7 +167,7 @@ def main() -> int:
 
     # currently_hunting = 'Azelf'
     # currently_hunting = 'Uxie'
-    currently_hunting = 'Regice'
+    currently_hunting = 'Suicune'
 
     x_val = 960
     y_val = 660
